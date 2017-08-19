61300c46f8df5009d467b42fd04974895fc92d35
Bryan Robbins <bryantrobbins@gmail.com>
2017-08-19 16:00:12 +0000

    Migrate from JS Fiddle

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/health', function (req, res) {
+  res.send('UP');
+});
+
+app.get('/svg', function (req, res) {
+  res.send('<svg width="300" height="200"> <rect width="100%" height="100%" fill="blue" /></svg>')
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('SVG App listening on port 3000')
 })
+
+app.use('/', express.static(path.join(__dirname, 'public')))
